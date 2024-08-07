d47ac48f2a59d58b88e2cd743f35f99222957bdc
Alejandro Ramirez <alejorp195@hotmail-com>
2024-08-07 00:13:16 -0500

    Add middleware solved issue

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -3,49 +3,44 @@ import { jwtDecode } from 'jwt-decode'
 
 interface IPermissions {
 
-		id: number;
-		canCreate: boolean;
-		canRead: boolean;
-		canUpdate: boolean;
-		canDelete: boolean;
-		roleId: number;
-		entityId: number;
-	}
+	id: number;
+	canCreate: boolean;
+	canRead: boolean;
+	canUpdate: boolean;
+	canDelete: boolean;
+	roleId: number;
+	entityId: number;
+}
+interface IToken {
+	id: number;
+	permissions: IPermissions[];
+}
+type TBoolPermissionArray = [boolean, boolean, boolean, boolean];
 
-	interface IToken {
-		id: number;
-		permissions: IPermissions[];
-	}
-	type TBoolPermissionArray = [boolean, boolean, boolean, boolean];
-
-	type TActionForbiden = 'canCreate' | 'canRead' | 'canUpdate' | 'canDelete';
+type TActionForbiden = 'canCreate' | 'canRead' | 'canUpdate' | 'canDelete';
 
 const authorize = (entityId: number, action: TActionForbiden) => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    const bearerToken = req.headers.authorization;
+	return (req: Request, res: Response, next: NextFunction) => {
+		const bearerToken = req.headers.authorization;
 		if (!bearerToken) {
 			return res.status(401).json({ message: 'User not authenticated' });
-		}	
+		}
 		const token = bearerToken.split(' ')[0];
-		const decoded:IToken = jwtDecode(token);
-		// console.log(decoded);
+		const decoded: IToken = jwtDecode(token);
 		const { permissions } = decoded;
-		console.log(permissions);
-		permissions.forEach((per: IPermissions) => {
-			if(per.entityId === entityId) {
-				if(per[action] === false) {
-					return res.status(403).json({ message: 'User does not have permission' });
+		const hasPermission = permissions.some((per: IPermissions) => {
+			if (per.entityId === entityId) {
+				if (per[action] === false) {
+					res.status(403).json({ message: 'User does not have permission' });
+					return true;
 				}
 			}
+			return false;
 		});
-		
-		
-		// if(!permissions[actionForbiden] && permissions.entityId === entityId) {
-		// 	return res.status(403).json({ message: 'User does not have permission' });
-		// }
-
-    next();
-  };
+		if (!hasPermission) {
+			next();
+		}
+	};
 };
 
 export default authorize;
